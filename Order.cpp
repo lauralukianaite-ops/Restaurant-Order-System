60d80d4e2f435ae9438a37c9543cc0f63938e16e
Laura <laura.lukianaite@gmail.com>
2026-05-15 18:35:47 +0300

    Deep copy implementation

---
+++ Order.cpp
@@ -0,0 +1,72 @@
+#include "Order.h"
+#include <vector>
+
+class OrderImpl {
+public:
+    std::vector<MenuItem*> items;
+    ~OrderImpl() {
+        for (MenuItem* item : items) {
+            delete item;
+        }
+        items.clear();
+    }
+};
+
+Order::Order() {
+    impl = new OrderImpl();
+}
+
+Order::~Order() {
+    delete impl;
+}
+
+// Deep Copy
+// Naudojamas Prototype šablonas
+Order::Order(const Order& other) {
+    impl = new OrderImpl();
+    for (MenuItem* item : other.impl->items) {
+        // Įsidedame ne tą pačią rodyklę, o gilią objekto kopiją
+        this->impl->items.push_back(item->clone()); 
+    }
+}
+
+// Priskyrimo operatorius (=) - irgi gilus kopijavimas
+Order& Order::operator=(const Order& other) {
+    if (this != &other) {
+        delete impl; // Išvalome senus duomenis
+        impl = new OrderImpl();
+        for (MenuItem* item : other.impl->items) {
+            this->impl->items.push_back(item->clone());
+        }
+    }
+    return *this;
+}
+
+// CRUD: Pridedamas elementas
+void Order::addItem(MenuItem* item) {
+    impl->items.push_back(item);
+}
+
+// CRUD: Sužinomas kiekis
+size_t Order::getSize() const {
+    return impl->items.size();
+}
+
+// CRUD: Gaunamas elementas pagal indeksą
+MenuItem* Order::getItem(size_t index) const {
+    if (index >= impl->items.size()) return nullptr;
+    return impl->items[index];
+}
+
+// 4. ITERATORIAUS METODAI
+// begin() grąžina iteratorių, rodantį į patį pirmą vektoriaus elementą
+Order::Iterator Order::begin() {
+    if (impl->items.empty()) return Iterator(nullptr);
+    return Iterator(&impl->items[0]);
+}
+
+// end() grąžina iteratorių, rodantį į vietą UŽ paskutinio elemento
+Order::Iterator Order::end() {
+    if (impl->items.empty()) return Iterator(nullptr);
+    return Iterator(&impl->items[0] + impl->items.size());
+}
